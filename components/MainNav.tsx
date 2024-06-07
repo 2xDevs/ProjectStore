f5a3c8a5070ccac812b98ab8d48d1d7a5658bcf2
vaaamsi1234 <vaaamsi1234@gmail.com>
2024-06-07 15:43:14 +0000

    initial commit

---
+++ components/MainNav.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/Icons"
+import { siteConfig } from "@/config/site"
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 flex ">
+                <Link href={""} className="mr-6 flex items-center space-x-1">
+                    <Icons.logo className="h-6 w-6 " />
+                    <span className="text-lg font-bold ">
+                        {siteConfig.name}
+                    </span>
+                </Link>
+                {/* <nav className="hidden md:flex items-center gap-4 text-sm lg:gap-6">
+                    <Link
+                        to="/docs"
+                        className={cn(
+                            "transition-colors hover:text-foreground/80",
+                            pathname === "/docs" ? "text-foreground" : "text-foreground/60"
+                        )}
+                    >
+                        Docs
+                    </Link>
+                    <Link
+                        to="/docs/components"
+                        className={cn(
+                            "transition-colors hover:text-foreground/80",
+                            pathname?.startsWith("/docs/components")
+                                ? "text-foreground"
+                                : "text-foreground/60"
+                        )}
+                    >
+                        Components
+                    </Link>
+                </nav> */}
+            </div>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "Project Store",
+    url: "#",
+    description:
+        "At ProjectsSelling, we provide high-quality, ready-made projects for college students across various disciplines. Enjoy affordable pricing and a user-friendly platform to find the perfect project for your academic needs.",
+    links: {
+        github: "https://github.com/parameshwar-arroju/RecapTube",
+    },
+}
+
+export type SiteConfig = typeof siteConfig
