2f5773d799dd9b98b48c3f43d690381c6db42f4a
parameshwar-arroju <parameshwarshiva119@gmail.com>
2024-06-09 20:21:54 +0530

    Added Logo

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -9,17 +9,17 @@ import { Icons } from "@/components/Icons"
 import { siteConfig } from "@/config/site"
 
 export function MainNav() {
-  const pathname = usePathname()
+    const pathname = usePathname()
 
-  return (
-    <div className="mr-4 flex ">
-                <Link href={""} className="mr-6 flex items-center space-x-1">
-                    <Icons.logo className="h-6 w-6 " />
-                    <span className="text-lg font-bold ">
-                        {siteConfig.name}
-                    </span>
-                </Link>
-                {/* <nav className="hidden md:flex items-center gap-4 text-sm lg:gap-6">
+    return (
+        <div className="mr-4 flex">
+            <Link href={""} className="mr-6 flex items-center space-x-1">
+                <Icons.logo className="h-12 w-12" />
+                <span className="text-2xl font-bold ">
+                    {siteConfig.name}
+                </span>
+            </Link>
+            {/* <nav className="hidden md:flex items-center gap-4 text-sm lg:gap-6">
                     <Link
                         to="/docs"
                         className={cn(
@@ -41,6 +41,6 @@ export function MainNav() {
                         Components
                     </Link>
                 </nav> */}
-            </div>
-  )
+        </div>
+    )
 }
