4b21c967861a123ffc76485519a0a7f4dfeaaa74
VamsiPunnaReddy <vaaamsi1234@gmail.com>
2024-06-11 23:44:51 +0530

    added profile page
    added Projects UserProfile components

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -31,6 +31,7 @@ export default function Profile() {
     ];
     return (
         <>
+        
             <div className="max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <UserProfile />
                 <Separator className="my-8" />
